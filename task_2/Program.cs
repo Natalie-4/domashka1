bcd2fd17a768a20ac0a0a46ea7bd0d99ec4d8312
Natalie <itlaym@yandex.ru>
2022-09-21 15:59:51 +0700

    create new folder task_2

---
+++ task_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+Console.Write("Введите число n1: ");
+double n1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите число n2: ");
+double n2 = Convert.ToDouble(Console.ReadLine());
+
+if (n1>n2)
+{
+    Console.WriteLine("n1 наибольшее число");
+    Console.Write("n2 наименьшее число");
+}
+else
+{
+    Console.WriteLine("n1 наименьшее число");
+    Console.Write("n2 наибольшее число");
+}
